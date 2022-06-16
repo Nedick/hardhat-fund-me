65e5a5ba41cf7fc897befee86255f6d423e83d3b
Nedelin Slavov <nedelin.slavov@gmail.com>
2022-06-16 18:22:20 +0300

    Added deployment script for fund-me contract

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,44 @@
+// function deployFunc(hre) {
+//     console.log("Hi!")
+//     hre.getNamedAccounts()
+//     hre.deployments
+// }
+
+// module.exports.default = deployFunc
+
+// module.exports = async (hre) => {
+// const { getNamedAccounts, deployments } = hre
+// }
+
+// const helperConfig = require("../helper-hardhat-config")
+// const networkConfig = helperConfig.networkConfig
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // if chainId is X use address Y
+    // if chainId is Z use address A
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    // if contract doesn't exist, we deploy a minimal version of our local testing
+
+    // when going for a localhost or hardhat network we want to use a mock
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress], // put price feed address
+        log: true,
+    })
+    log("----------------------------------------------------")
+}
+module.exports.tags = ["all", "fundme"]
